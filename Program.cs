72dc5c0f8139a8f16991a56aa3cfb6cb7ce61810
josuecarata <caratajosue@yahoo.com>
2024-04-21 21:03:21 -0300

    Hasta Ejercicio 2

--- Program.cs
+++ Program.cs
@@ -10,6 +10,32 @@ namespace Ejercicio_01
     {
         static void Main(string[] args)
         {
+            Console.Title = "Ejercicio Nro 01";
+            int numeroIngresado;
+            int numeroMayor = int.MinValue;
+            int numeroMenor = int.MaxValue;
+            int numeroTotal = 0;
+            float promedio = 0;
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Ingrese numero");
+                numeroIngresado =   int.Parse(Console.ReadLine());
+                if(numeroIngresado > numeroMayor)
+                {
+                    numeroMayor = numeroIngresado;
+                }
+                else if(numeroIngresado < numeroMenor)
+                {
+                    numeroMenor = numeroIngresado;
+                }
+                numeroTotal = numeroTotal + numeroIngresado;
+            }
+            promedio = (float)(numeroTotal / 5d);
+            Console.WriteLine("Numero mayor:{0}", numeroMayor);
+            Console.WriteLine("Numero menor:{0}", numeroMenor);
+            Console.WriteLine("Numero total: {0}", numeroTotal);
+            Console.WriteLine("Promedio: {0:#.###,00}", promedio);
+            Console.ReadKey();
         }
     }
 }
